d5610fea8d2d2d62c26ebe6338cb17b09e50ba8b
Craig Mulligan <hobochildster@gmail.com>
2018-06-16 00:15:35 +0100

    add direction + haptic feed back on complete

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Constants, Location, Permissions } from 'expo'
+import { Constants, Location, Permissions, Haptic } from 'expo'
 import { StyleSheet, Text, View } from 'react-native'
 import Places from 'google-places-web'
-import Geo from 'geodesy'
+import { LatLonSpherical as Geo } from 'geodesy'
+import { Ionicons } from '@expo/vector-icons'
 
-Places.apiKey = 'AIzaSyCneSbDKqwD6g4agqJJOzphPUXvyoT1Bsw'
+Places.apiKey = ''
 Places.debug = __DEV__ // boolean;
 
 export default class App extends React.Component {
@@ -12,11 +13,11 @@ export default class App extends React.Component {
     super()
     this.state = {
       target: {},
+      bearing: 0,
     }
   }
 
   componentDidMount = async () => {
-    console.log('mounted')
     let { status } = await Permissions.askAsync(Permissions.LOCATION)
     if (status !== 'granted') {
       this.setState({
@@ -33,25 +34,63 @@ export default class App extends React.Component {
         type: ['cafe'],
       })
 
-      this.setState({ target: places[0] })
+      this.setState({ target: places[0] }, () => {
+        this._bearing()
+      })
     } catch (err) {
       console.log(err)
     }
   }
 
   _bearing() {
-    const latLng = new Geo(
-      this.state.location.coords.latitude,
-      this.state.location.coords.longitude,
-    )
-    const bearing = latLng.bearingTo(this.state.target.geometry.location)
-    return bearing
+    Expo.Location.watchHeadingAsync(({ trueHeading }) => {
+      const latLng = new Geo(
+        this.state.location.coords.latitude,
+        this.state.location.coords.longitude,
+      )
+
+      const targetLocale = this.state.target.geometry.location
+      const to = new Geo(targetLocale.lat, targetLocale.lng)
+      const bearing = latLng.bearingTo(to)
+
+      this.setState({
+        bearing: bearing - trueHeading,
+        distance: latLng.distanceTo(to),
+      })
+
+      return bearing
+    })
+  }
+
+  _areWeThereYet() {
+    if (this.state.distance < 20) {
+      this.setState(
+        {
+          journey: {
+            complete: true,
+          },
+        },
+        () => {
+          Haptic.notification(Haptic.NotificationTypes.Success)
+        },
+      )
+    }
+  }
+
+  _style() {
+    return StyleSheet.create({
+      arrow: {
+        transform: [{ rotateZ: `${this.state.bearing}deg` }],
+      },
+    })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>{JSON.stringify(this.state.target)}</Text>
+        <Text>{this.state.bearing}</Text>
+        <Text>{this.state.distance}</Text>
+        <Ionicons style={this._style().arrow} name="md-arrow-up" size={500} />
       </View>
     )
   }
@@ -63,5 +102,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  arrow: {
+    transform: [{ rotateZ: '45deg' }],
   },
 })
